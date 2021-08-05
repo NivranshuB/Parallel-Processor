f59261db5b218f83d97a6871c0c749198ad4c316
sv-singh <ssin610@aucklanduni.ac.nz>
2021-08-05 17:46:33 +1200

    Use arg parser in main function

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -4,10 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 
+        ArgumentParser parser = new ArgumentParser();
+		Config config;
+		
+		try {
+			config = parser.parse(args);
+		} catch (IllegalArgumentException e) {
+			System.out.println("Unable to parse arguments"+ e.getMessage());
+			return;
+		}
+
+        System.out.println("input file = " + config.getInputFile());
+		System.out.println("number of processors = " + config.getNumOfProcessors());
+		System.out.println("number of cores = " + config.getNumOfCores());
+		System.out.println("visualise = " + config.getVisualise());
+		System.out.println("output file = " + config.getOutputFile());
 
 
     }
-
-
 
 }
