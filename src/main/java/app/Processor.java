e3ff18330ff9b0fb843505874c1d094b305fc7c4
NivranshuB <nbos433@aucklanduni.ac.nz>
2021-08-05 20:48:24 +1200

    Refactored Processor class to use Node type instead of Task type and added some comments to Task class and Task_Dependency class

--- src/main/java/app/Processor.java
+++ src/main/java/app/Processor.java
@@ -1,16 +1,17 @@
 package app;
 
 import java.util.List;
+import model.Node;
 
 /**
  * Author: Team UNTESTED
  * This class represents the ordered lists of nodes for a particular processor of
- * a particular schedule. It also stores the time that this processor is on after all
+ * a particular schedule. It also stores the time that this processor is at after all
  * its tasks have been finished.
  */
 public class Processor {
     
-    public List<Task> task_order;
+    public List<Node> task_order;
     public int finish_time;
 
     public void assign_task(Task task) {
