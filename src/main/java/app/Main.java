1e1497a1cdf53ab638056791a9bac9f80369c61b
NivranshuB <nbos433@aucklanduni.ac.nz>
2021-08-05 20:15:50 +1200

    Added skeleton code to the Main class

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,11 +1,25 @@
 package app;
 
+import com.sun.org.apache.xpath.internal.objects.XNumber;
+
+/**
+ * Author: Team UNTESTED
+ * Main method where the implementation of the program begins.
+ */
 public class Main {
 
     public static void main(String[] args) {
+        //SV's code to parse command line input arguments to extract the number of
+        //processors, DOT file and other args
 
+        //Joe's code that uses the DOT filepath from the args to create a new
+        //DotFileReader object/graph representation
 
+        Scheduler scheduler = Scheduler.getInstance();
 
+        //optimalSchedule = scheduler.getOptimalSchedule(nodeMap, edgeMap, numberOfProcessors);
+
+        //Corban's code to parse the optimal schedule to the output DOT file
     }
 
 
