82f629f64239f6849126822a2ac79f4b0ab0c00b
ckar364 <ckar364@aucklanduni.ac.nz>
2021-08-07 13:24:32 +1200

    Added JavaDoc method information for Node class.

--- src/main/java/model/Node.java
+++ src/main/java/model/Node.java
@@ -4,10 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Class to represent the nodes in the graph, representing the tasks to be scheduled. Each node can have 0 to many
+ * Class to represent the Nodes in the graph, representing the tasks to be scheduled. Each node can have 0 to many
  * parent Nodes, and 0 to many child Nodes. Each node also has a corresponding weight.
  */
-
 public class Node {
 
     private List<Node> parent = new ArrayList<Node>();
@@ -16,31 +15,63 @@ public class Node {
     private int start;
     private int processor;
 
+    /**
+     * Gets the parent nodes of this node.
+     * @return Parent nodes of this node.
+     */
     public List<Node> getParent() {
         return parent;
     }
 
+    /**
+     * Adds a parent node of this node.
+     * @param parentNode Parent node of this node.
+     */
     public void addParent(Node parentNode) {
         parent.add(parentNode);
     }
 
+    /**
+     * Gets the child nodes of this node.
+     * @return Child nodes of this node.
+     */
     public List<Node> getChild() {
         return child;
     }
 
+    /**
+     * Adds a child node of this node.
+     * @param childNode Child node of this node.
+     */
     public void addChild(Node childNode) {
         child.add(childNode);
     }
 
+    /**
+     * Gets the weight of this node.
+     * @return Weight of this node.
+     */
     public int getWeight() {
         return weight;
     }
 
+    /**
+     * Sets the weight of this node.
+     * @param weight Weight of this node.
+     */
     public void setWeight(int weight) {
         this.weight = weight;
     }
 
+    /**
+     * Gets the start time of when this node is computed.
+     * @return Start time when node is computed.
+     */
     public int getStart(){ return start; }
 
+    /**
+     * Gets the processor this node is scheduled on.
+     * @return Processor number this node is scheduled on.
+     */
     public int getProcessor(){ return processor; }
 }
