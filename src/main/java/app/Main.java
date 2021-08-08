f805f0e4bdc2c141cc02f963a00951468933fb70
SV Singh <ssin610@aucklanduni.ac.nz>
2021-08-08 13:48:36 +1200

    Fixed code indentation

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -9,23 +9,23 @@ public class Main {
     public static void main(String[] args) {
         //Parses command line input arguments to extract the number of processors, DOT file and other args
         ArgumentParser parser = new ArgumentParser();
-		Config config;
+	Config config;
 		
-		try {
-			config = parser.parse(args);
-		} catch (IllegalArgumentException e) {
-			System.out.println("Unable to parse arguments "+ e.getMessage());
-			return;
-		}
+	try {
+		config = parser.parse(args);
+	} catch (IllegalArgumentException e) {
+		System.out.println("Unable to parse arguments "+ e.getMessage());
+		return;
+	}
       
-		Scheduler scheduler = Scheduler.getInstance();
+	Scheduler scheduler = Scheduler.getInstance();
 
-		//Uses the DOT filepath from the args to create a new DotFileReader object/graph representation
-		DotFileReader dotFileReader = new DotFileReader(config.getInputFile());
+	//Uses the DOT filepath from the args to create a new DotFileReader object/graph representation
+	DotFileReader dotFileReader = new DotFileReader(config.getInputFile());
 
         Schedule optimalSchedule = scheduler.getOptimalSchedule(dotFileReader.getNodeMap(), dotFileReader.getEdgeMap(), config.getNumOfProcessors());
 
-		String graphName = dotFileReader.getGraphName();
+	String graphName = dotFileReader.getGraphName();
 
         //Parses the optimal schedule to the output DOT file
         OutputParser op = new OutputParser(graphName, config, optimalSchedule);
