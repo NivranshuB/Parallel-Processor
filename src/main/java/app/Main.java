4a38fbc8e2245ea77c4ec14f5332bd438dda1b05
ckar364 <ckar364@aucklanduni.ac.nz>
2021-08-08 12:08:32 +1200

    Improved comments written in the Main class.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -7,9 +7,7 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
-        //SV's code to parse command line input arguments to extract the number of
-        //processors, DOT file and other args
-
+        //Parses command line input arguments to extract the number of processors, DOT file and other args
         ArgumentParser parser = new ArgumentParser();
 		Config config;
 		
@@ -22,16 +20,14 @@ public class Main {
       
 		Scheduler scheduler = Scheduler.getInstance();
 
-		//Joe's code that uses the DOT filepath from the args to create a new
-		//DotFileReader object/graph representation
-
+		//Uses the DOT filepath from the args to create a new DotFileReader object/graph representation
 		DotFileReader dotFileReader = new DotFileReader(config.getInputFile());
 
         Schedule optimalSchedule = scheduler.getOptimalSchedule(dotFileReader.getNodeMap(), dotFileReader.getEdgeMap(), config.getNumOfProcessors());
 
 		String graphName = dotFileReader.getGraphName();
 
-        //Corban's code to parse the optimal schedule to the output DOT file
+        //Parses the optimal schedule to the output DOT file
         OutputParser op = new OutputParser(graphName, config, optimalSchedule);
 
         op.writeFile();
