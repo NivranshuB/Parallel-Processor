f8560ced9799eb2157cc06d33051d471535f6794
Corban Draper <cdra334@aucklanduni.ac.nz>
2021-08-15 13:08:09 +1200

    feat shows number of processors in javafx controller

--- src/main/java/app/MainController.java
+++ src/main/java/app/MainController.java
@@ -1,5 +1,18 @@
 package app;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+
 public class MainController {
+
+    @FXML
+    private Label numOfProcessors;
+
+    public void initialize() {
+        Config config = Config.getInstance();
+        int numOfP = config.getNumOfProcessors();
+        numOfProcessors.setText(String.valueOf(numOfP));
+    }
     
 }
