aca235cbf8d01b8018db1891ef3dd6f618dee7ff
Corban Draper <cdra334@aucklanduni.ac.nz>
2021-08-15 14:46:53 +1200

    feat displays number of cores on GUI

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -6,6 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.net.URL;
+import java.nio.file.Paths;
+
 /**
  * Author: Team UNTESTED
  * Main method where the implementation of the program begins.
@@ -14,25 +17,27 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         //Parses command line input arguments to extract the number of processors, DOT file and other args
-		launch(args);
-        ArgumentParser parser = new ArgumentParser();
-	Config config;
-		
-	try {
-		config = parser.parse(args);
-	} catch (IllegalArgumentException e) {
-		System.out.println("Unable to parse arguments "+ e.getMessage());
-		return;
-	}
-      
-	Scheduler scheduler = Scheduler.getInstance();
 
-	//Uses the DOT filepath from the args to create a new DotFileReader object/graph representation
-	DotFileReader dotFileReader = new DotFileReader(config.getInputFile());
+        ArgumentParser parser = new ArgumentParser();
+        Config config;
+
+        try {
+            config = parser.parse(args);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Unable to parse arguments " + e.getMessage());
+            return;
+        }
+
+        Scheduler scheduler = Scheduler.getInstance();
+
+        //Uses the DOT filepath from the args to create a new DotFileReader object/graph representation
+        DotFileReader dotFileReader = new DotFileReader(config.getInputFile());
+
+        launch(args);
 
         Schedule optimalSchedule = scheduler.getOptimalSchedule(dotFileReader.getNodeMap(), dotFileReader.getEdgeMap(), config.getNumOfProcessors());
 
-	String graphName = dotFileReader.getGraphName();
+        String graphName = dotFileReader.getGraphName();
 
         //Parses the optimal schedule to the output DOT file
         OutputParser op = new OutputParser(graphName, config, optimalSchedule);
@@ -40,13 +45,15 @@ public class Main extends Application {
         op.writeFile();
     }
 
-	@Override
-	public void start(Stage arg0) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+    @Override
+    public void start(Stage arg0) throws Exception {
+        URL url = Paths.get("./src/main/java/app/Main.fxml").toUri().toURL();
+        Parent root = FXMLLoader.load(url);
+//		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
         arg0.setTitle("Task Scheduler");
         arg0.setScene(new Scene(root));
         arg0.show();
-		
-	}
+
+    }
 
 }
--- src/main/java/app/MainController.java
+++ src/main/java/app/MainController.java
@@ -7,12 +7,22 @@ import javafx.scene.text.Text;
 public class MainController {
 
     @FXML
+    private Label numOfTasks;
+
+    @FXML
     private Label numOfProcessors;
+
+    @FXML
+    private Label numOfCores;
 
     public void initialize() {
         Config config = Config.getInstance();
+
         int numOfP = config.getNumOfProcessors();
+        int numOfC = config.getNumOfCores();
+
         numOfProcessors.setText(String.valueOf(numOfP));
+        numOfCores.setText(String.valueOf(numOfC));
     }
     
 }
