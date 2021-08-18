74693d50f3b62961f01c0cafc103c9c3bcbc8765
NivranshuB <nbos433@aucklanduni.ac.nz>
2021-08-19 08:57:29 +1200

    Added SystemTests.java in the src/test/java directory and added skeleton code for system level testing in this class.

---
+++ src/test/java/SystemTests.java
@@ -0,0 +1,158 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class SystemTests {
+
+    /**
+     * This sets up an Edge object for testing.
+     */
+    @Before
+    public void testSetUp() {
+
+    }
+
+    /**
+     * Tests for 4 node input graph, one processor
+     */
+    @Test
+    public void NodesFourProcessorOneTest() {
+
+    }
+
+    /**
+     * Tests for 4 node input graph, two processor
+     */
+    @Test
+    public void NodesFourProcessorTwoTest() {
+
+    }
+
+    /**
+     * Tests for 4 node input graph, four processor
+     */
+    @Test
+    public void NodesFourProcessorFourTest() {
+
+    }
+
+    /**
+     * Tests for 7 node input graph, single processor
+     */
+    @Test
+    public void NodesSevenProcessorOneTest() {
+
+    }
+
+    /**
+     * Tests for 7 node input graph, two processor
+     */
+    @Test
+    public void NodesSevenProcessorTwoTest() {
+
+    }
+
+    /**
+     * Tests for 7 node input graph, four processor
+     */
+    @Test
+    public void NodesSevenProcessorFourTest() {
+
+    }
+
+    /**
+     * Tests for 8 node input graph, single processor
+     */
+    @Test
+    public void NodesEightProcessorOneTest() {
+
+    }
+
+    /**
+     * Tests for 8 node input graph, two processor
+     */
+    @Test
+    public void NodesEightProcessorTwoTest() {
+
+    }
+
+    /**
+     * Tests for 8 node input graph, four processor
+     */
+    @Test
+    public void NodesEightProcessorFourTest() {
+
+    }
+
+    /**
+     * Tests for 9 node input graph, single processor
+     */
+    @Test
+    public void NodesNineProcessorOneTest() {
+
+    }
+
+    /**
+     * Tests for 9 node input graph, two processor
+     */
+    @Test
+    public void NodesNineProcessorTwoTest() {
+
+    }
+
+    /**
+     * Tests for 9 node input graph, four processor
+     */
+    @Test
+    public void NodesNineProcessorFourTest() {
+
+    }
+
+    /**
+     * Tests for 10 node input graph, single processor
+     */
+    @Test
+    public void NodesTenProcessorOneTest() {
+
+    }
+
+    /**
+     * Tests for 10 node input graph, two processor
+     */
+    @Test
+    public void NodesTenProcessorTwoTest() {
+
+    }
+
+    /**
+     * Tests for 10 node input graph, four processor
+     */
+    @Test
+    public void NodesTenProcessorFourTest() {
+
+    }
+
+    /**
+     * Tests for 11 node input graph, single processor
+     */
+    @Test
+    public void NodesElevenProcessorOneTest() {
+
+    }
+
+    /**
+     * Tests for 11 node input graph, two processor
+     */
+    @Test
+    public void NodesElevenProcessorTwoTest() {
+
+    }
+
+    /**
+     * Tests for 11 node input graph, four processor
+     */
+    @Test
+    public void NodesElevenProcessorFourTest() {
+
+    }
+
+}
