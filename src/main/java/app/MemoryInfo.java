9ecafdc182ec8c7cc404ad0a6663a8875bd2636c
sv-singh <ssin610@aucklanduni.ac.nz>
2021-08-22 21:23:10 +1200

    Reduced height of UI

--- src/main/java/app/MemoryInfo.java
+++ src/main/java/app/MemoryInfo.java
@@ -11,44 +11,44 @@ public class MemoryInfo implements Runnable {
 	private int interval;
 	private MainController controller;
 
-	 /**
-     * Constructs the MemoryInfo object used to monitor and push memory usage.
-     * @param controller UI controller.
-     * @param interval   The interval to push updates.
-     */
+	/**
+	 * Constructs the MemoryInfo object used to monitor and push memory usage.
+	 * @param controller UI controller.
+	 * @param interval   The interval to push updates.
+	 */
 	public MemoryInfo(MainController controller, int interval)   {
 		this.interval = interval;
 		this.controller = controller;
-		
+
 		// Launch CPUInfo in a different thread.
-        this.cpuInfo = new Thread(new CPUInfo(this.controller));
-        this.cpuInfo.start();
+		this.cpuInfo = new Thread(new CPUInfo(this.controller));
+		this.cpuInfo.start();
 	}
 
 	/**
-     * Start the MemoryInfo thread and push memory usage to the main controller.
-     */
+	 * Start the MemoryInfo thread and push memory usage to the main controller.
+	 */
 	public void run() {
-        while (true)    {
-            long start = System.currentTimeMillis();
-            if (Thread.currentThread().isInterrupted()) {
-                cpuInfo.interrupt();
-                break;
-            }
+		while (true)    {
+			long start = System.currentTimeMillis();
+			if (Thread.currentThread().isInterrupted()) {
+				cpuInfo.interrupt();
+				break;
+			}
 
-            Runtime runtime = Runtime.getRuntime();
-            controller.updateMemory(runtime.totalMemory() - runtime.freeMemory());
-            
-            // Sleep for the given interval
-            try {
-                long sleep = interval - (System.currentTimeMillis() - start);
-                Thread.sleep(sleep > 0 ? sleep : 1);
-            } catch (InterruptedException e)    {
-                cpuInfo.interrupt();
-                break;
-            }
+			Runtime runtime = Runtime.getRuntime();
+			controller.updateMemory(runtime.totalMemory() - runtime.freeMemory());
 
-        }
-    }
+			// Sleep for the given interval
+			try {
+				long sleep = interval - (System.currentTimeMillis() - start);
+				Thread.sleep(sleep > 0 ? sleep : 1);
+			} catch (InterruptedException e)    {
+				cpuInfo.interrupt();
+				break;
+			}
+
+		}
+	}
 
 }
