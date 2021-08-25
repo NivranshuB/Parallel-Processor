00c11f2c41d00ac273dd67dfa6e421534622bd87
Joe Cheong <j.cheong@hotmail.com>
2021-08-25 20:55:38 +1200

    FIX NumberFormatException due to parsing empty string into int in addListener()

--- src/main/java/app/MainController.java
+++ src/main/java/app/MainController.java
@@ -332,6 +332,9 @@ public class MainController {
                             // looks through potential optimal schedule of each thread
                             for (String n : lastOptimalNode) {
                                 n = n.split("_")[0];
+                                if (n.equals("")) {
+                                    n = "0";
+                                }
                                 // checks that new value is less than all threads
                                 if (Integer.parseInt(n) < (int) (evt.getNewValue())) {
                                     newOptimal = false;
