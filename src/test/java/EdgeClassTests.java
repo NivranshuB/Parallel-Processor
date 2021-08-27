56cdf8388b4ac42b9f0760a7e5d7d79e948bea3d
ckar364 <ckar364@aucklanduni.ac.nz>
2021-08-28 11:11:27 +1200

    Added more tests to test the Edge class.

--- src/test/java/EdgeClassTests.java
+++ src/test/java/EdgeClassTests.java
@@ -3,8 +3,7 @@ import model.Node;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
+import static org.junit.Assert.*;
 
 /**
  * This class is used to unit test the methods in the Edge class.
@@ -15,6 +14,9 @@ public class EdgeClassTests {
     private static Node parentNode;
     private static Node childNode;
     private static final int EDGE_WEIGHT = 6;
+
+    private static Node newParentNode;
+    private static Node newChildNode;
     private static final int NEW_EDGE_WEIGHT = 10;
 
     /**
@@ -25,10 +27,21 @@ public class EdgeClassTests {
         parentNode = new Node();
         childNode = new Node();
 
+        parentNode.setName("Parent 1");
+        childNode.setName("Child 1");
+
+        newParentNode = new Node();
+        newChildNode = new Node();
+
+        newParentNode.setName("Parent 2");
+        newChildNode.setName("Child 2");
+
         testEdge = new Edge();
         testEdge.setParentNode(parentNode);
         testEdge.setChildNode(childNode);
         testEdge.setWeight(EDGE_WEIGHT);
+
+
     }
 
     /**
@@ -87,5 +100,72 @@ public class EdgeClassTests {
         testEdge.setWeight(NEW_EDGE_WEIGHT);
 
         assertEquals(NEW_EDGE_WEIGHT, testEdge.getWeight());
+    }
+
+    /**
+     * This test checks to see if the equals method in Edge class returns the correct boolean when
+     * an object to be compared is passed in. In this case, the object passed in is exactly the same
+     * object it is being compared to.
+     */
+    @Test
+    public void testEqualsSameObject() {
+        assertTrue(testEdge.equals(testEdge));
+    }
+
+    /**
+     * This test checks to see if the equals method in Edge class returns the correct boolean when
+     * an object to be compared is passed in. In this case, the object passed in is not an Edge object.
+     */
+    @Test
+    public void testEqualsNotEdgeObject() {
+        assertFalse(testEdge.equals(new Node()));
+    }
+
+    /**
+     * This test checks to see if the equals method in Edge class returns the correct boolean when
+     * an object to be compared is passed in. In this case, the object passed in is an Edge with a
+     * different parent.
+     */
+    @Test
+    public void testEqualsDifferentParentEdge() {
+        Edge secondTestEdge = new Edge();
+
+        secondTestEdge.setParentNode(newParentNode);
+        secondTestEdge.setChildNode(childNode);
+        secondTestEdge.setWeight(EDGE_WEIGHT);
+
+        assertFalse(testEdge.equals(secondTestEdge));
+    }
+
+    /**
+     * This test checks to see if the equals method in Edge class returns the correct boolean when
+     * an object to be compared is passed in. In this case, the object passed in is an Edge with a
+     * different child.
+     */
+    @Test
+    public void testEqualsDifferentChildEdge() {
+        Edge secondTestEdge = new Edge();
+
+        secondTestEdge.setParentNode(parentNode);
+        secondTestEdge.setChildNode(newChildNode);
+        secondTestEdge.setWeight(EDGE_WEIGHT);
+
+        assertFalse(testEdge.equals(secondTestEdge));
+    }
+
+    /**
+     * This test checks to see if the equals method in Edge class returns the correct boolean when
+     * an object to be compared is passed in. In this case, the object passed in is an Edge with a
+     * different weight.
+     */
+    @Test
+    public void testEqualsDifferentWeightEdge() {
+        Edge secondTestEdge = new Edge();
+
+        secondTestEdge.setParentNode(parentNode);
+        secondTestEdge.setChildNode(childNode);
+        secondTestEdge.setWeight(NEW_EDGE_WEIGHT);
+
+        assertFalse(testEdge.equals(secondTestEdge));
     }
 }
